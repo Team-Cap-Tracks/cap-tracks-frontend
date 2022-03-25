93bb78d683e165cfd65fb7593faf45b31d7d62b2
MarcusLind42 <96486666+MarcusLind42@users.noreply.github.com>
2022-03-25 15:47:06 -0400

    Adjusting NavBar icons

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -10,6 +10,7 @@ const NavBar = ({ user, handleLogout }) => {
           <ul className='ulclass'>
             {/* <li>Welcome, {user.name}</li> */}
             <li><i class="bi bi-arrow-left-square-fill"></i></li>
+            <li><i class="bi bi-map"></i></li>
             <li><Link to="/profiles"><p><i class="bi bi-person-circle"></i></p></Link></li>
             {/* <li><Link to="" onClick={handleLogout}>LOG OUT</Link></li>
             <li><Link to="/changePassword">Change Password</Link></li> */}
