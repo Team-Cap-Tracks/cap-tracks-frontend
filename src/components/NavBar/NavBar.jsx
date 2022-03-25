c1546add8e63ce3520141792c308e97ff31e6143
MarcusLind42 <96486666+MarcusLind42@users.noreply.github.com>
2022-03-25 15:26:12 -0400

    Adding profile icon to navbar

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -9,7 +9,7 @@ const NavBar = ({ user, handleLogout }) => {
         <nav>
           <ul>
             <li>Welcome, {user.name}</li>
-            <li><Link to="/profiles">Profiles</Link></li>
+            <li><Link to="/profiles"><p><i class="bi bi-person-circle"></i></p></Link></li>
             <li><Link to="" onClick={handleLogout}>LOG OUT</Link></li>
             <li><Link to="/changePassword">Change Password</Link></li>
           </ul>
