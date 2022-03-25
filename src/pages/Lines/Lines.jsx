ae3aadfec4e7cb0f3f24fa8306806ddb0a748c34
Caleb Vietinghoff <vietinghoffcaleb@gmail.com>
2022-03-25 16:34:40 -0400

    print all lines to page

--- src/pages/Lines/Lines.jsx
+++ src/pages/Lines/Lines.jsx
@@ -1,11 +1,11 @@
 const Lines = (props) => {
-  console.log(props.lines[0].line);
-  console.log(props);
+  console.log(props.lines);
+
   return (
     <>
     <h1>Lines</h1>
     {props.lines.map(line => {
-      <p>{props.lines.line}</p>
+      return <p>{line.line}</p>
     })}
     </>
   )
