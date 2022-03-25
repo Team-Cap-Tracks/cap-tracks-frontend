2a2be348954f87aa3144f6176fb622d5f48d3390
GravleyCM <GravleyCM@gmail.com>
2022-03-25 14:50:09 -0400

    add Link to landing

--- src/pages/Landing/Landing.jsx
+++ src/pages/Landing/Landing.jsx
@@ -1,10 +1,12 @@
 import styles from './Landing.module.css'
+import { Link } from 'react-router-dom'
 
 const Landing = ({ user }) => {
   return (
     <main className={styles.container}>
-      <h1>hello, {user ? user.name : 'friend'}</h1>
+      {/* <h1>hello, {user ? user.name : 'friend'}</h1> */}
       <img src="https://www.wmata.com/about/news/images/2021-Inauguration-System-Map-1_1.jpg" alt="" />
+      <button><Link to="/lines">To Metro Lines</Link></button>
     </main>
   )
 }
