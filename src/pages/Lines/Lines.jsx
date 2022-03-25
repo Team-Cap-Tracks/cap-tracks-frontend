6803f3122d9ff901bc9ed38a84b8219285162619
Caleb Vietinghoff <vietinghoffcaleb@gmail.com>
2022-03-25 16:26:41 -0400

    troubleshoot printing lines to page

--- src/pages/Lines/Lines.jsx
+++ src/pages/Lines/Lines.jsx
@@ -1,11 +1,12 @@
 const Lines = (props) => {
-  console.log(props.lines);
+  console.log(props.lines[0].line);
+  console.log(props);
   return (
     <>
     <h1>Lines</h1>
-    {props.lines.map(line => (
-      <p>{props.lines}</p>
-    ))}
+    {props.lines.map(line => {
+      <p>{props.lines.line}</p>
+    })}
     </>
   )
 }
