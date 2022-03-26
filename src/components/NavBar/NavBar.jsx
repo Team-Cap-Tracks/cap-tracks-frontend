7e4d244222f32d57720321ee25514641bafc8582
MarcusLind42 <96486666+MarcusLind42@users.noreply.github.com>
2022-03-26 15:05:06 -0400

    Styling navbar if no user

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -8,18 +8,17 @@ const NavBar = ({ user, handleLogout }) => {
       {user ?
         <nav>
           <ul className='ulclass'>
-            {/* <li>Welcome, {user.name}</li> */}
             <li><i className="bi bi-arrow-left-square-fill"></i></li>
             <li>Cap Tracks</li>
             <li><i className="bi bi-map"></i></li>
             <li><Link to="/profiles"><i className="bi bi-person-circle"></i></Link></li>
-            {/* <li><Link to="" onClick={handleLogout}>LOG OUT</Link></li>
-            <li><Link to="/changePassword">Change Password</Link></li> */}
           </ul>
         </nav>
       :
         <nav>
-          <ul>
+          <ul className='ulclass-no-user'>
+            <li><i className="bi bi-arrow-left-square-fill"></i></li>
+            <li>Cap Tracks</li>
             <li><Link to="/login">Log In</Link></li>
             <li><Link to="/signup">Sign Up</Link></li>
           </ul>
