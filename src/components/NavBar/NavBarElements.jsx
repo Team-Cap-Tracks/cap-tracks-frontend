6dd739a3eb63ff37007b57d2d047d76645788573
MarcusLind42 <96486666+MarcusLind42@users.noreply.github.com>
2022-03-26 19:25:02 -0400

    Adding hover transition to navbar

--- src/components/NavBar/NavBarElements.jsx
+++ src/components/NavBar/NavBarElements.jsx
@@ -1,13 +1,14 @@
 import styled from 'styled-components'
 import { NavLink as Link } from 'react-router-dom'
+import {FaBars} from 'react-icons/fa'
 
 export const Nav = styled.nav`
   background: #272643;
-  height: 8vh;
+  height: 10vh;
   display: flex;
   justify-content: space-between;
   padding: .5rem calc((100vw - 1000px) / 2);
-  z-index: 10
+  z-index: 10;
 `
 
 export const NavLink = styled(Link)`
@@ -21,22 +22,46 @@ export const NavLink = styled(Link)`
   padding: 0 1rem;
   height: 100%;
   cursor: pointer;
+  transition: all 0.2s ease-in-out;
+
+  &:hover {
+    transition: all 0.2s ease-in-out;
+  }
 `
 export const NavLinkLeft = styled(Link)`
   color: #fff;
   display: flex;
   align-items: center;
   text-decoration: none;
-  padding: 0 1rem;
+  padding: 0 .5rem;
   height: 100%;
   cursor: pointer;
 `
 
 export const NavMenu = styled.div`
-  
   display: flex;
   align-items: center;
   margin-right: -24px;
+
+  @media screen and (max-width: 768px) {
+    display: none;
+  }
 `
+
+export const Bars = styled(FaBars)`
+  display: none;
+  color: #fff;
+
+  @media screen and (max-width: 768px) {
+    display: block;
+    position: absolute;
+    top: 0;
+    right: 0;
+    transform: translate(-100%, 75%);
+    font-size: 1.8rem;
+    cursor: pointer;
+  }  
+`
+
 
 
