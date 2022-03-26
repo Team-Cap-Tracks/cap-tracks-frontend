dbc9b30a4cdfbdf775be186cc1b1feb08a1ca1ca
MarcusLind42 <96486666+MarcusLind42@users.noreply.github.com>
2022-03-26 14:22:09 -0400

    Troubleshooting profile.jsx logic

--- src/pages/Profiles/Profiles.jsx
+++ src/pages/Profiles/Profiles.jsx
@@ -1,7 +1,8 @@
 import { useState, useEffect } from 'react'
 import * as profileService from '../../services/profileService'
+import { Link } from 'react-router-dom'
 
-const Profiles = (user, handleLogout) => {
+const Profiles = ({ user, handleLogout }) => {
   const [profiles, setProfiles] = useState([])
 
   useEffect(()=> {
@@ -11,13 +12,16 @@ const Profiles = (user, handleLogout) => {
 
   return (
     <>
-      <h1>Hello. This is a list of all the profiles.</h1>
+      <h1><li>Welcome, {}</li></h1>
+      <li><Link to="" onClick={handleLogout}>LOG OUT</Link></li>
+            <li><Link to="/changePassword">Change Password</Link></li>
       {profiles.length ? 
         <>
           {profiles.map(profile=>
             <p key={profile._id}>{profile.name}</p>
           )}
         </>
+      
       :
         <p>No profiles yet</p>
       }
