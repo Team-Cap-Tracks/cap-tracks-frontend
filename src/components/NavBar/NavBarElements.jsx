d66da1fc3913a25b95860b2496c3aad2e05ce048
MarcusLind42 <96486666+MarcusLind42@users.noreply.github.com>
2022-03-26 19:35:02 -0400

    Removing Hamburger

--- src/components/NavBar/NavBarElements.jsx
+++ src/components/NavBar/NavBarElements.jsx
@@ -1,6 +1,6 @@
 import styled from 'styled-components'
 import { NavLink as Link } from 'react-router-dom'
-import {FaBars} from 'react-icons/fa'
+
 
 export const Nav = styled.nav`
   background: #272643;
@@ -42,26 +42,8 @@ export const NavMenu = styled.div`
   display: flex;
   align-items: center;
   margin-right: -24px;
-
-  @media screen and (max-width: 768px) {
-    display: none;
-  }
 `
 
-export const Bars = styled(FaBars)`
-  display: none;
-  color: #fff;
-
-  @media screen and (max-width: 768px) {
-    display: block;
-    position: absolute;
-    top: 0;
-    right: 0;
-    transform: translate(-100%, 75%);
-    font-size: 1.8rem;
-    cursor: pointer;
-  }  
-`
 
 
 
