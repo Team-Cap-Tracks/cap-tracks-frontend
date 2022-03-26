a53eb5fb7be67e8816e046bc5ef2acb5c83181e1
Hanibee <cyborgofthefuture@gmail.com>
2022-03-26 15:51:57 -0400

    styling for card

--- src/components/LinesCard/LinesCard.jsx
+++ src/components/LinesCard/LinesCard.jsx
@@ -1,3 +1,7 @@
+import { useState } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
+import styles from './LinesCard.module.css'
+import * as authService from '../../services/authService'
 
 const LinesCard = ({ lines }) => {
   return (
