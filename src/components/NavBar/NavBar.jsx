c1a1dc12efcfa5b7cf04e648e7753fe2f52c8731
MarcusLind42 <96486666+MarcusLind42@users.noreply.github.com>
2022-03-26 16:16:58 -0400

    Building out material UI test

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,34 +1,34 @@
 import styled from 'styled-components'
 import { Link } from 'react-router-dom'
-import {FaBars} from 'react-icons/fa'
 
 
-const NavBar = ({ user, handleLogout }) => {
-  return (
-    <>
-      <header className="Nav-Bar">
-      {user ?
-        <nav>
-          <ul className='ulclass'>
-            <li><i className="bi bi-arrow-left-square-fill"></i></li>
-            <li>Cap Tracks</li>
-            <li><i className="bi bi-map"></i></li>
-            <li><Link to="/profiles"><i className="bi bi-person-circle"></i></Link></li>
-          </ul>
-        </nav>
-      :
-        <nav>
-          <ul className='ulclass-no-user'>
-            <li><i className="bi bi-arrow-left-square-fill"></i></li>
-            <li>Cap Tracks</li>
-            <li><Link to="/login">Log In</Link></li>
-            <li><Link to="/signup">Sign Up</Link></li>
-          </ul>
-        </nav>
-      }
-      </header>
-    </>
-  )
-}
 
-export default NavBar
+export const Nav = styled.nav`
+  background: #000;
+  height: 80px;
+  display: flex;
+  justify-content: space-between;
+  padding: 0.5rem calc((100vw - 1000px) / 2);
+  z-index: 10`
+
+export const NavLink = styled(Link)`
+  color: #fff;
+  display: flex;
+  align-items: center;
+  text-decoration: none;
+  padding: 0 1rem;
+  height: 100%;
+  cursor: pointer;
+    
+  &.active {
+    color: #15cdfc;
+  }
+`
+
+export const NavMenu = styled.div`
+  display: flex;
+  align-items: center;
+  margin-right: -24px;
+  `
+
+
