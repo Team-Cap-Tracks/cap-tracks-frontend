9eec05038834a9ea16ead21e5ec7eff5c3490ac0
MarcusLind42 <96486666+MarcusLind42@users.noreply.github.com>
2022-03-26 17:03:55 -0400

    Adding ternary function to NavBar

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -1,25 +1,31 @@
 import React from 'react'
 import { Nav, NavLink, NavLinkLeft, NavMenu } from './NavBarElements'
 
-const NavBar = () => {
+const NavBar = ({user}) => {
   return (
     <>
-      <Nav>
-        <NavLinkLeft to='/back' activeStyle>
-          <i className="bi bi-arrow-left-square-fill"></i>
-        </NavLinkLeft>
-        <NavLink to='/'>
-          <h1>Cap Tracks</h1>
-        </NavLink>
-        <NavMenu>
-          <NavLink to='/map' activeStyle>
-            <i className="bi bi-map"></i>
+      {user ?
+        <Nav>
+          <NavLinkLeft to='/back' activeStyle>
+            <i className="bi bi-arrow-left-square-fill"></i>
+          </NavLinkLeft>
+          <NavLink to='/'>
+            <h1>Cap Tracks</h1>
           </NavLink>
-          <NavLink to='/profiles' activeStyle>
-            <i className="bi bi-person-circle"></i>
-          </NavLink>
-        </NavMenu>
-      </Nav>
+          <NavMenu>
+            <NavLink to='/map' activeStyle>
+              <i className="bi bi-map"></i>
+            </NavLink>
+            <NavLink to='/profiles' activeStyle>
+              <i className="bi bi-person-circle"></i>
+            </NavLink>
+          </NavMenu>
+        </Nav>
+      :
+        <Nav>
+
+        </Nav>
+      }
     </>
   )
 }
