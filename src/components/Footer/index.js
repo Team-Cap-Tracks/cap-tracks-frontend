70e48640c474ff836d88e0d12604db194c7452c9
MarcusLind42 <96486666+MarcusLind42@users.noreply.github.com>
2022-03-27 19:46:34 -0400

    Stubbing out footer wrapper fn

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,3 +1,9 @@
 import React from "react";
 
-export default function Footer({ children, ...restProps})
+export default function Footer({ children, ...restProps}) {
+  return <Container {...restProps}>{children}</Container>
+}
+
+Footer.Wrapper = function FooterWrapper({ children, ...restProps}) {
+  
+}
