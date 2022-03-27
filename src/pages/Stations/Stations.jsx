7338341388f728d8fbc9b19f7a8dca6bd858d972
GravleyCM <GravleyCM@gmail.com>
2022-03-27 13:18:53 -0400

    refactoring code

--- src/pages/Stations/Stations.jsx
+++ src/pages/Stations/Stations.jsx
@@ -1,7 +1,10 @@
 const StationsList = ({ lines }) => {
-  console.log(lines.stations)
+  // console.log(lines[0].Stations[0].Name)
  return (
-  <h1>Stations</h1>
+   <>
+    <h1>Stations</h1>
+    <p>{lines[0].Stations[0].Name}</p>
+  </>
 )
 }
 
