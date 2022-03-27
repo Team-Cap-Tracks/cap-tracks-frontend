21921a83d75b8107ec370f719cfe583d912fc407
Caleb Vietinghoff <vietinghoffcaleb@gmail.com>
2022-03-27 13:53:40 -0400

    troubleshoot station page

--- src/pages/Stations/Stations.jsx
+++ src/pages/Stations/Stations.jsx
@@ -1,9 +1,13 @@
-const StationsList = ({ lines }) => {
+const StationsList = (props) => {
   // console.log(lines[0].Stations[0].Name)
  return (
   <>
     <h1>Stations</h1>
-    <p>{lines[0].Stations[0].Name}</p>
+    {/* <p>{lines[0].Stations[0].Name}</p> */}
+    {props.lines.map(station => {
+      return {station}
+    })}
+
   </>
 )
 }
