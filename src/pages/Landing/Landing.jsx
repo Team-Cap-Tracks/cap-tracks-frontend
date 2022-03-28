57447ab0686ffa6c075bb7cc074f2caad72f213b
Hanibee <cyborgofthefuture@gmail.com>
2022-03-28 11:31:10 -0400

    transition on station lines buttons

--- src/pages/Landing/Landing.jsx
+++ src/pages/Landing/Landing.jsx
@@ -6,7 +6,11 @@ const Landing = ({ user }) => {
     <main className={styles.container}>
       {/* <h1>hello, {user ? user.name : 'friend'}</h1> */}
       <img src="https://i.imgur.com/bHoOQWE.jpg" alt="DC Metro Map" />
-      <button><Link to="/lines">To Metro Lines</Link></button>
+      <br></br>
+      <Link to="/lines">
+      <button>To Metro Lines</button>
+      </Link>
+      <br></br>
     </main>
   )
 }
