99c3af1d02dde09805d4d2cf3981702051d09ce9
MarcusLind42 <96486666+MarcusLind42@users.noreply.github.com>
2022-03-28 13:58:54 -0400

    Commiting before pr

--- src/components/StationDetailsCard/StationDetailsCard.jsx
+++ src/components/StationDetailsCard/StationDetailsCard.jsx
@@ -6,10 +6,10 @@ const StationDetailsCard = (props) => {
       <div className="stationDetailsCard">
         <p className="stationDetails">
           {props.stationInfo.state.stationList.Name}<br />
-          {props.stationInfo.state.stationList.Address.City}
-          {props.stationInfo.state.stationList.Address.State}
-          {props.stationInfo.state.stationList.Address.Street}
-          {props.stationInfo.state.stationList.Address.Zip}
+          {props.stationInfo.state.stationList.Address.City}<br />
+          {props.stationInfo.state.stationList.Address.State}<br />
+          {props.stationInfo.state.stationList.Address.Street}<br />
+          {props.stationInfo.state.stationList.Address.Zip}<br />
 
         </p>
       </div>
