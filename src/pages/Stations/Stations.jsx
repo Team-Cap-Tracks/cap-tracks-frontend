60f392b09adb98b1db140797c532eec6e5255d91
Hanibee <cyborgofthefuture@gmail.com>
2022-03-28 09:57:06 -0400

    button styling on landing page

--- src/pages/Stations/Stations.jsx
+++ src/pages/Stations/Stations.jsx
@@ -2,8 +2,10 @@ const StationsList = ({ lines }) => {
   // console.log(lines[0].Stations[0].Name)
  return (
   <>
+  <div class="station">
     <h1>Stations</h1>
     <p>{lines[0].Stations[0].Name}</p>
+    </div>
   </>
 )
 }
