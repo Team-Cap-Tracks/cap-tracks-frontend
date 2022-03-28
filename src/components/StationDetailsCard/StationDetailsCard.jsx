8347c0c3b3c88629a96282f1ab81c362493cb688
MarcusLind42 <96486666+MarcusLind42@users.noreply.github.com>
2022-03-28 13:19:19 -0400

    Stubbing out station details card

--- src/components/StationDetailsCard/StationDetailsCard.jsx
+++ src/components/StationDetailsCard/StationDetailsCard.jsx
@@ -2,8 +2,13 @@ const StationDetailsCard = (props) => {
 
   return (
     <>
-      <div className="stationdetailscard">
-        <p className="stationdetails">Yo</p>
+      <div className="stationDetailsCard">
+        <p className="stationDetails">
+          {props.stationList.Address.City}
+          {props.stationList.Address.State}
+          {props.stationList.Address.Street}
+          {props.stationList.Address.Zip}
+        </p>
       </div>
     </>
   )
