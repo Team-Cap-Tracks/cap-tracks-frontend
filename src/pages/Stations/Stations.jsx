f4303bb515e553d0da7e020cb7dacac2006f55f0
MarcusLind42 <96486666+MarcusLind42@users.noreply.github.com>
2022-03-28 12:43:11 -0400

    Editing station details

--- src/pages/Stations/Stations.jsx
+++ src/pages/Stations/Stations.jsx
@@ -9,7 +9,7 @@ const StationsList = (props) => {
       {location.state.line.Stations.map(stationList => {
         return (
           <>
-            <h3>{stationList.Name}, {stationList.Code}</h3>
+            <h3><strong>{stationList.Name}</strong></h3>
             {stationList.Address.City}
             {stationList.Address.State}
             {stationList.Address.Street}
