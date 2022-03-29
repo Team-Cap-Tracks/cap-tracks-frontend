b355ccf17935ad4477445300ad1a0f6c2d3b19e3
Caleb Vietinghoff <vietinghoffcaleb@gmail.com>
2022-03-29 13:49:36 -0400

    add breaks into timetable

--- src/components/TimeTable/TimeTable.jsx
+++ src/components/TimeTable/TimeTable.jsx
@@ -2,10 +2,16 @@ import { useLocation } from 'react-router-dom'
 
 const TimeTable = (props) => {
   let location = useLocation()
-  console.log(location.state);
+  // console.log(location.state.stationList.Code);
+  console.log(props.state.timeTables.Code);
   return (
     <>
       <h1>Station Schedule</h1>
+      {props.state.timeTables.map(timetable => {
+        <>
+          {timetable.Code}
+        </>
+      })}
     </>
 
   )
