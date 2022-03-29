1b585e7d68645d1d1df2f4c06a5d4b820675b471
Caleb Vietinghoff <vietinghoffcaleb@gmail.com>
2022-03-29 12:56:05 -0400

    try mapping through timetables

--- src/pages/StationDetails/StationDetails.jsx
+++ src/pages/StationDetails/StationDetails.jsx
@@ -4,13 +4,15 @@ import styles from './StationDetails.module.css'
 
 const StationDetails = ({timeTables}) => {
   let location = useLocation()
-  console.log(timeTables)
+  console.log(location)
+  console.log(...timeTables);
   return (
     <>
       <h1 className={styles.station}>Station Details</h1>
       <StationDetailsCard className={styles.stationdetails}
         stationInfo={location}/>
     </>
+
   )
 }
 
