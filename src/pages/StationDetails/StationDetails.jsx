abf5b952362e3fb7521b3f88744b0dfe6f4922bf
GravleyCM <GravleyCM@gmail.com>
2022-03-30 14:27:37 -0400

    remove code that in not needed

--- src/pages/StationDetails/StationDetails.jsx
+++ src/pages/StationDetails/StationDetails.jsx
@@ -1,18 +1,10 @@
-// import { useEffect } from 'react'
 import { useLocation } from "react-router-dom";
 import StationDetailsCard from '../../components/StationDetailsCard/StationDetailsCard'
 import TimeTable from "../../components/TimeTable/TimeTable";
 import styles from './StationDetails.module.css'
-// import * as stationTimings from '../../services/TimeTables'
 
 const StationDetails = ({timeTables}) => {
   let location = useLocation()
-  // let { code } = useParams()
-
-  // useEffect(() => {
-  //   stationTimings.getAllTimes(code)
-  //   .then(res => console.log(res)) 
-  // })
 
   return (
     <>
@@ -21,7 +13,6 @@ const StationDetails = ({timeTables}) => {
         stationInfo={location}/>
       <TimeTable state={{timeTables}} />
     </>
-
   )
 }
 
