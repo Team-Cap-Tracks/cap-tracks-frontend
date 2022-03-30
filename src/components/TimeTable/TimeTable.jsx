67816d2007c7c855b681a03c68df414c01023a34
GravleyCM <GravleyCM@gmail.com>
2022-03-30 13:45:29 -0400

    add last trains to monday-friday

--- src/components/TimeTable/TimeTable.jsx
+++ src/components/TimeTable/TimeTable.jsx
@@ -9,17 +9,29 @@ const TimeTable = (props) => {
   useEffect(() => {
     stationTimings.getAllTimes(code)
     .then(res => setSchedule(res.StationTimes[0]))
-  }, [])
+  }, [code])
 
   return (
     <>
       <h1>Station Schedule</h1>
+      {/* Monday */}
       <h2>Monday - Friday: Opens at {schedule?.Monday?.OpeningTime} am</h2>
+      {/* First Trains */}
       <p>First Trains: {schedule?.Monday?.FirstTrains[0]?.Time} am from {schedule?.Monday?.FirstTrains[0]?.DestinationStation} {schedule?.Monday?.FirstTrains[1]?.DestinationStation} {schedule?.Monday?.FirstTrains[2]?.DestinationStation} {schedule?.Monday?.FirstTrains[3]?.DestinationStation} {schedule?.Monday?.FirstTrains[4]?.DestinationStation}</p>
+      {/* Last Trains */}
+      <p>Last Trains: {schedule?.Monday?.LastTrains[0]?.Time} am from {schedule?.Monday?.LastTrains[0]?.DestinationStation} {schedule?.Monday?.LastTrains[1]?.DestinationStation} {schedule?.Monday?.LastTrains[2]?.DestinationStation} {schedule?.Monday?.LastTrains[3]?.DestinationStation} {schedule?.Monday?.LastTrains[4]?.DestinationStation}</p>
+
+      {/* Saturday */}
       <h2>Saturday: Opens at {schedule?.Saturday?.OpeningTime} am</h2>
+      {/* First Trains */}
       <p>{schedule?.Saturday?.FirstTrains[0]?.Time} am from {schedule?.Saturday?.FirstTrains[0]?.DestinationStation} {schedule?.Saturday?.FirstTrains[1]?.DestinationStation} {schedule?.Saturday?.FirstTrains[2]?.DestinationStation} {schedule?.Saturday?.FirstTrains[3]?.DestinationStation} {schedule?.Saturday?.FirstTrains[4]?.DestinationStation}</p>
+      {/* Last Trains */}
+
+      {/* Sunday */}
       <h2>Sunday: Opens at {schedule?.Sunday?.OpeningTime} am</h2>
+      {/* First Trains */}
       <p>First Trains: {schedule?.Sunday?.FirstTrains[0]?.Time} am from {schedule?.Sunday?.FirstTrains[0]?.DestinationStation} {schedule?.Sunday?.FirstTrains[1]?.DestinationStation} {schedule?.Sunday?.FirstTrains[2]?.DestinationStation} {schedule?.Sunday?.FirstTrains[3]?.DestinationStation} {schedule?.Monday?.FirstTrains[4]?.DestinationStation}</p>
+      {/* Last Trains */}
     </>
   )
 }
