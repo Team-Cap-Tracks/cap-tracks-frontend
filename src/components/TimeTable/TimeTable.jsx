8a9ace73b5f87bae3b5819e9926d801c6b05e43f
GravleyCM <GravleyCM@gmail.com>
2022-03-30 13:06:37 -0400

    add more first trains and destination stations

--- src/components/TimeTable/TimeTable.jsx
+++ src/components/TimeTable/TimeTable.jsx
@@ -17,6 +17,7 @@ const TimeTable = (props) => {
       <h1>Station Schedule</h1>
       <p>Monday - Friday: Opens at {schedule?.Monday?.OpeningTime} am</p>
       <p>First Trains: {schedule?.Monday?.FirstTrains[0].Time} am from {schedule?.Monday?.FirstTrains[0].DestinationStation}</p>
+      <p>First Trains: {schedule?.Monday?.FirstTrains[0].Time} am from {schedule?.Monday?.FirstTrains[1].DestinationStation}</p>
       <p>Saturday: Opens at {schedule?.Saturday?.OpeningTime} am</p>
       <p>Sunday: Opens at {schedule?.Sunday?.OpeningTime} am</p>
     </>
