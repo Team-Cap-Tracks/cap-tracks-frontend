df2031de90416e37093fc92b271d7b9f3189986b
Hanibee <cyborgofthefuture@gmail.com>
2022-03-30 10:39:26 -0400

    ticket button

--- src/pages/Profiles/Profiles.jsx
+++ src/pages/Profiles/Profiles.jsx
@@ -5,11 +5,14 @@ const Profiles = ({ user, handleLogout }) => {
   return (
     <>
       {user ?
+      <>
       <ul>
       <li className={styles.welcome}>Welcome, {user.name}</li>
       <li><Link to="" onClick={handleLogout}>LOG OUT</Link></li>
       <li><Link to="/changePassword">Change Password</Link></li>
       </ul>
+      <button>Create Ticket</button>
+      </>
       :
       <h1>No User</h1>
       }
