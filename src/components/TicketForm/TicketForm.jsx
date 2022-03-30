d5eeeb908759b0964e06874c7e53412bbe8f927f
Caleb Vietinghoff <vietinghoffcaleb@gmail.com>
2022-03-30 15:13:07 -0400

    start refactor of form

--- src/components/TicketForm/TicketForm.jsx
+++ src/components/TicketForm/TicketForm.jsx
@@ -30,37 +30,17 @@ const TicketForm = ({lines}) => {
     <>
         <div>Ticket Form</div>
         <form>
-            <select  className="form-select form-select-lg mb-3" aria-label=".form-select-lg example">
+            <select 
+            name='Lines' 
+            onChange={event => handleLineSelect(event)} 
+            value={selectedLine} 
+            className="form-select form-select-lg mb-3" 
+            aria-label=".form-select-lg example">
                 Origin: 
-                {lines.map(lines => { 
-                    return (
-                        <option value=''>{lines.line}</option>
-                    )
-                })}
+                <option value=''>Select the Line</option>
+                {lineList.map(())}
             </select>
-            {selectedLine ? 
-            <select className="form-select form-select-lg mb-3" aria-label=".form-select-lg example">
-                {lines[selectedLine].map(allStations => {
-                    return <option>{allStations}</option>
-                })}
-                
-            </select>
-            :
-            <select className="form-select form-select-lg mb-3" aria-label=".form-select-lg example">
 
-            </select>
-}
-            <select className="form-select form-select-lg mb-3" aria-label=".form-select-lg example">
-                Destination:
-                {lines.map(lines => { 
-                    return (
-                        <option >{lines.line}</option>
-                    )
-                })}
-            </select>
-            <select className="form-select form-select-lg mb-3" aria-label=".form-select-lg example">
-                
-            </select>
             <button>Create Ticket</button>
         </form>
     </>
