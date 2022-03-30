c0f705f7e188a65e77163afb35e17d1ef401f788
GravleyCM <GravleyCM@gmail.com>
2022-03-30 11:53:29 -0400

    at ? for drilling conditions

--- src/components/TimeTable/TimeTable.jsx
+++ src/components/TimeTable/TimeTable.jsx
@@ -8,15 +8,16 @@ const TimeTable = (props) => {
 
   useEffect(() => {
     stationTimings.getAllTimes(code)
-    .then(res => setSchedule(res.StationTimes[0].Monday))
+    .then(res => setSchedule(res.StationTimes[0]))
   }, [])
 
   console.log("Scheduling", schedule)
   return (
     <>
       <h1>Station Schedule</h1>
-      <p>Monday - Friday: {schedule.OpeningTime}</p>
-      <p>Monday - Friday: {schedule.OpeningTime}</p>
+      <p>Monday - Friday: Opens at {schedule?.Monday?.OpeningTime}</p>
+      <p>Saturday: Opens at {schedule?.Saturday?.OpeningTime}</p>
+      <p>Sunday: Opens at {schedule?.Sunday?.OpeningTime}</p>
     </>
 
   )
