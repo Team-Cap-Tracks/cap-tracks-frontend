bffbb5e47cf6db2a2934ce8d5ad3d4c9b4bad979
Caleb Vietinghoff <vietinghoffcaleb@gmail.com>
2022-03-31 08:38:27 -0400

    stub up create on front end

--- src/services/ticketService.js
+++ src/services/ticketService.js
@@ -1,2 +1,18 @@
 import * as tokenService from './tokenService'
 const BASE_URL = `${process.env.REACT_APP_BACKEND_SERVER_URL}/api/tickets`
+
+function create(ticket) {
+  return fetch(BASE_URL, {
+    method: 'POST',
+    headers: {
+      'content-type': 'application/json',
+      'Authorization': `Bearer ${tokenService.getToken()}`
+    },
+    body: JSON.stringify(ticket)
+  })
+  .then(res => res.json())
+}
+
+export {
+  create
+}
