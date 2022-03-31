d9f86541ef9d99636706378684c11fc2dad618ce
GravleyCM <GravleyCM@gmail.com>
2022-03-31 14:11:48 -0400

    drill tickets prop

--- src/pages/Profiles/Profiles.jsx
+++ src/pages/Profiles/Profiles.jsx
@@ -3,7 +3,7 @@ import styles from './Profiles.module.css'
 import TicketForm from '../../components/TicketForm/TicketForm'
 import TicketList from '../../components/TicketList/TicketList'
 
-const Profiles = ({ user, handleLogout, lines, handleAddTicket }) => {
+const Profiles = ({ user, handleLogout, lines, handleAddTicket, tickets }) => {
   return (
     <>
       {user ?
@@ -13,7 +13,7 @@ const Profiles = ({ user, handleLogout, lines, handleAddTicket }) => {
       <li><Link to="" onClick={handleLogout}>LOG OUT</Link></li>
       <li><Link to="/changePassword">Change Password</Link></li>
       </ul>
-      <TicketList />
+      <TicketList tickets={tickets}/>
       <TicketForm lines={lines} handleAddTicket={handleAddTicket}/>
       </>
       :
