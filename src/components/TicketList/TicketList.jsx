c0b9f8b6f89a3293bf41de9ae9644d731fc5af5f
GravleyCM <GravleyCM@gmail.com>
2022-03-31 14:27:51 -0400

    import TicketCard

--- src/components/TicketList/TicketList.jsx
+++ src/components/TicketList/TicketList.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
+import TicketCard from '../TicketCard/TicketCard'
 
 const TicketList = (props) => {
   return (
     <>
+    <TicketCard />
     {props.tickets.map(ticket => (
      <p>{ticket.startStation}</p>
     ))}
