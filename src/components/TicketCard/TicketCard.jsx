0b010246e52cbb8f2458779af7c00858b35d25f4
MarcusLind42 <96486666+MarcusLind42@users.noreply.github.com>
2022-03-31 21:18:32 -0400

    Styling ticket list

--- src/components/TicketCard/TicketCard.jsx
+++ src/components/TicketCard/TicketCard.jsx
@@ -1,18 +1,21 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import styles from './TicketCard.module.css'
 
 const TicketCard = ({ticket, handleDelete, lines, handleUpdateTicket}) => {
   return (
     <>
-    <p>Origin: {ticket.startStation} Destination: {ticket.endStation}</p>
-    <button onClick={() => handleDelete(ticket._id)}>Delete Ticket</button>
-    <Link 
-      to='/editTicket'
-      state={{ticket}}
-      
-    >
-      <button>Update Ticket</button>
-    </Link>
+    <div className={styles.ticketcard}>
+      <p>Origin: {ticket.startStation} Destination: {ticket.endStation}</p>
+      <button onClick={() => handleDelete(ticket._id)}>Delete Ticket</button>
+      <Link 
+        to='/editTicket'
+        state={{ticket}}
+        
+      >
+        <button>Update Ticket</button>
+      </Link>
+    </div>
     </>
   )
 }
