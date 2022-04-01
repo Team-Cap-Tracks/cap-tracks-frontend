f35fb4f4dc6691b804364c389f8b225d241aaa42
MarcusLind42 <96486666+MarcusLind42@users.noreply.github.com>
2022-03-31 20:34:09 -0400

    Adding grid

--- src/pages/Profiles/Profiles.jsx
+++ src/pages/Profiles/Profiles.jsx
@@ -8,15 +8,20 @@ const Profiles = ({ user, handleLogout, lines, handleAddTicket, tickets, handleD
     <>
       {user ?
       <>
-      <ul>
-      <li className={styles.welcome}>Welcome, {user.name}</li>
-      <li><Link to="" onClick={handleLogout}>LOG OUT</Link></li>
-      <li><Link to="/changePassword">Change Password</Link></li>
-      </ul>
-      <TicketForm lines={lines} handleAddTicket={handleAddTicket}/>
-      <br />
-      <br />
-      <TicketList tickets={tickets} handleDelete={handleDelete} lines={lines} handleUpdateTicket={handleUpdateTicket}/>
+      <div className="container">
+        <div className='welcome'>
+          <ul>
+            <li className={styles.welcome}>Welcome, {user.name}</li>
+            <li><Link to="" onClick={handleLogout}>LOG OUT</Link></li>
+            <li><Link to="/changePassword">Change Password</Link></li>
+          </ul>
+        </div>
+        <div className=''
+        <TicketForm lines={lines} handleAddTicket={handleAddTicket}/>
+        <br />
+        <br />
+        <TicketList tickets={tickets} handleDelete={handleDelete} lines={lines} handleUpdateTicket={handleUpdateTicket}/>
+      </div>
       </>
       :
       <h1>No User</h1>
