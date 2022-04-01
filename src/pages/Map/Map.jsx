4228919fa3d3f1d1b8767d191f4772ac4100748d
GravleyCM <GravleyCM@gmail.com>
2022-03-31 21:15:48 -0400

    add styling to map img

--- src/pages/Map/Map.jsx
+++ src/pages/Map/Map.jsx
@@ -4,6 +4,7 @@ export const Map = () => {
   return (
     <img
     src='https://i.imgur.com/bHoOQWE.jpg' alt='A map of the DC metro system'
+    style={{height: "70vh", width: "70vw"}}
     />
   )
 }
