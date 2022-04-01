6a60151bebce6625e5b9ea79aec466fc59f88f5d
MarcusLind42 <96486666+MarcusLind42@users.noreply.github.com>
2022-03-31 21:02:15 -0400

    Adding flexbox to profiles

--- src/pages/Profiles/Profiles.jsx
+++ src/pages/Profiles/Profiles.jsx
@@ -6,21 +6,25 @@ import TicketList from '../../components/TicketList/TicketList'
 const Profiles = ({ user, handleLogout, lines, handleAddTicket, tickets, handleDelete, handleUpdateTicket }) => {
   return (
     <>
-      {user ?
-      <>
-      <ul>
-      <li className={styles.welcome}>Welcome, {user.name}</li>
-      <li><Link to="" onClick={handleLogout}>LOG OUT</Link></li>
-      <li><Link to="/changePassword">Change Password</Link></li>
-      </ul>
-      <TicketForm lines={lines} handleAddTicket={handleAddTicket}/>
-      <br />
-      <br />
-      <TicketList tickets={tickets} handleDelete={handleDelete} lines={lines} handleUpdateTicket={handleUpdateTicket}/>
-      </>
-      :
-      <h1>No User</h1>
-      }
+      <main>
+        {user ?
+        <>
+        <section>
+          <ul>
+            <li className={styles.welcome}>Welcome, {user.name}</li>
+            <li><Link to="" onClick={handleLogout}>LOG OUT</Link></li>
+            <li><Link to="/changePassword">Change Password</Link></li>
+          </ul>
+        </section>
+        <TicketForm lines={lines} handleAddTicket={handleAddTicket}/>
+        <br />
+        <br />
+        <TicketList tickets={tickets} handleDelete={handleDelete} lines={lines} handleUpdateTicket={handleUpdateTicket}/>
+        </>
+        :
+        <h1>No User</h1>
+        }
+      </main>
     </>
   )
 }
